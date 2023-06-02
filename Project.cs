cdb03b1ffca34670c9ae75a7030d75dd79df3dad
Ertan Muslic <ertanmuslic@gmail.com>
2023-06-03 00:13:03 +0200

    Added Class for Project Creation

---
+++ Project.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasker
+{
+    internal class Project
+    {
+        public string name {  get; set; }
+        public string goal { get; set; }
+        public DateTime deadline { get; set; }
+        public Project(string n,string g,DateTime d)
+        {
+            this.name = n;
+            this.goal = g; 
+            this.deadline = d;
+        }
+    }
+}

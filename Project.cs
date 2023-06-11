c2a0bd48ab5b78a4ffad12388bfce18fff2f5962
Ertan Muslic <ertanmuslic@gmail.com>
2023-06-11 18:21:17 +0200

    Added INotifyPropertyChanged event to name property

--- Project.cs
+++ Project.cs
@@ -1,19 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Tasker
 {
-    public class Project
+    public class Project: INotifyPropertyChanged
     {
-        public string Name {  get; set; }
+        private string name;
+        public string Name {
+
+            get {
+                return name;
+            } set
+            {
+                if(name != value)
+                {
+                    name = value;
+                    NotifyPropertyChanged("Name");
+                }
+            } 
+        }
         public string Goal { get; set; }
         public DateTime Deadline { get; set; }
 
         public ObservableCollection<Task> Tasks { get; set; }
-        
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(string propertyName) //Notifies us whenever the property of Name (in this case) is changed.Used to update names in ComboBox
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }

3a961230fd58461a0126848cacb1c975083d767f
Ertan Muslic <ertanmuslic@gmail.com>
2023-06-11 15:46:55 +0200

    Link Projects with Tasks

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -17,6 +17,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Xml.Linq;
+using Microsoft.VisualStudio.CommandBars;
 
 namespace Tasker
 {
@@ -30,21 +31,25 @@ namespace Tasker
 
         public ObservableCollection<Task> Tasks { get; set; }
 
-
         public ObservableCollection<string> Members { get; set; }
+
+        public int SelectedIndex = 0; // Index of the element that will be selected when app starts for the first time
+
         public MainWindow()
         {
             InitializeComponent();
+
+            
             Projects = new ObservableCollection<Project>()
             {
                 new Project {Name = "Create Project",Goal="",Deadline = new DateTime(6/12/2000)}
             };
 
-            cbx.ItemsSource = Projects;
+            cbx.ItemsSource = Projects; //Bind Projects List to ComboBox 
+            cbx.SelectedIndex = SelectedIndex; // Initialy ComboBox will point to the "Create Project" which is on index 0
 
-            //DataContext = this; //Bind Projects List to ComboBox 
 
-            Tasks = new ObservableCollection<Task>()
+            Tasks = new ObservableCollection<Task>() //Will be deleted shortly
             {
                 new Task {Name = "Name1",Description ="Description1", Priority=1, Date=new DateTime(2000,1,1), Members = new List < string >() { "Select Memeber","Ramiz","Ertan"}},
                 new Task {Name = "Name2",Description ="Description2", Priority=1, Date=new DateTime(2000,1,1), Members = new List < string >() { "Select Member","Ramiz","Ertan" }},
@@ -61,7 +66,7 @@ namespace Tasker
 
             };
 
-            taskList.ItemsSource = Tasks; // Bind taskList to Tasks (ItemsControl)
+            taskList.ItemsSource = Projects[SelectedIndex].Tasks; // Bind taskList to Projects[Index].Tasks(all tasks in the List) (ItemsControl)
 
 
         }
@@ -80,8 +85,9 @@ namespace Tasker
 
             if (name != "" && goal != "" && deadline != DateTime.MinValue) // If second window was closed manualy,empty project will be created hence this if statement
             {
-                Project project = new Project { Name = name, Goal = goal, Deadline = deadline };
-                Projects.Add(project);
+                Project project = new Project { Name = name, Goal = goal, Deadline = deadline , Tasks = new ObservableCollection<Task>()};
+                Projects.Add(project); //Add new Project
+                cbx.SelectedIndex++; //Select newly created Project
                 MessageBox.Show($"Successfuly created Project: {project.Name}");
             }
 
@@ -96,10 +102,10 @@ namespace Tasker
 
         private void Create_Task(object sender, RoutedEventArgs e)
         {
-            CreateTaskWindow newTask = new CreateTaskWindow();
+            CreateTaskWindow newTask = new CreateTaskWindow(); //Same logic for Creating Project applies here
             newTask.ShowDialog();
 
-
+            SelectedIndex = cbx.SelectedIndex; //Get Index of the currently selected project where the Tasks will be added
             string name = newTask.name.Text;
             string desc = newTask.description.Text;
             int priority = newTask.Priority;
@@ -109,9 +115,15 @@ namespace Tasker
             if(name != "" && desc != "" && priority != 0 && date != DateTime.MinValue)
             {
                 Task task = new Task { Name=name,Description =desc , Priority = priority , Date = date ,Members = members};
-                Tasks.Add(task);
+                Projects[SelectedIndex].Tasks.Add(task); //Add newly created Task to the selected Project
                 MessageBox.Show($"Successfully created Task: {task.Name}");
             }
+            taskList.ItemsSource = Projects[SelectedIndex].Tasks; //Bind ItemsControl to the selected Project to show all Tasks for that specific Project
+        }
+
+        private void cbx_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            taskList.ItemsSource = Projects[cbx.SelectedIndex].Tasks; // Shen selection is changed remove previous Tasks and add show Tasks for newly selected Project
         }
     }
 }
--- Project.cs
+++ Project.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,8 @@ namespace Tasker
         public string Name {  get; set; }
         public string Goal { get; set; }
         public DateTime Deadline { get; set; }
+
+        public ObservableCollection<Task> Tasks { get; set; }
         
     }
 }
